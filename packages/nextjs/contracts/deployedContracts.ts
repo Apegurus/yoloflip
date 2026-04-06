6e93f7a0a784873a9982ebc0cb8f940fc9c7acac
Apeguru <hello@apeguru.dev>
2026-04-05 23:24:24 -0300

    chore: verify and clean up YoloFlip implementation

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     YoloFlip: {
-      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
       abi: [
         {
           inputs: [
@@ -176,6 +176,11 @@ const deployedContracts = {
         {
           inputs: [],
           name: "ReentrancyGuardReentrantCall",
+          type: "error",
+        },
+        {
+          inputs: [],
+          name: "TransferFailed",
           type: "error",
         },
         {
@@ -900,7 +905,7 @@ const deployedContracts = {
         supportsInterface: "@openzeppelin/contracts/access/AccessControl.sol",
         paused: "@openzeppelin/contracts/utils/Pausable.sol",
       },
-      deployedOnBlock: 6,
+      deployedOnBlock: 1,
     },
   },
 } as const;
